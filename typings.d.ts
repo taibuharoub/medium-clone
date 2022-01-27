1cae06ddd8b733c45aa8dc35b5207599afe3dfae
Taibu Haroub <haroubtaibu@gmail.com>
2022-01-28 01:58:33 +0300

    Deploy

--- typings.d.ts
+++ typings.d.ts
@@ -9,6 +9,7 @@ export interface Post {
         name: string;
         image: string;
     }
+    comments: [Comment];
     description: string;
     mainImage: {
         asset: {
@@ -21,3 +22,18 @@ export interface Post {
     body: [object]
 }
 
+export interface Comment {
+    approved: boolean;
+    comment: string;
+    email: string;
+    name: string;
+    post: {
+        _ref: string;
+        _type: string;
+    }
+    _createdAt: string;
+    _id: string;
+    _rev: string;
+    _type: string;
+    _updatedAt: string;
+}
